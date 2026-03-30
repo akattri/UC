6588d978cf0aec9da733f85ffc58c15450a138d0
Akash <akashattri62@gmail.com>
2026-03-30 21:32:38 +0530

    uc8

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,57 +1,41 @@
 public class OOPSBannerApp {
-    private static class CharacterPattern {
-        private final char character;
-        private final String[] pattern;
+    private static final java.util.Map<Character, String[]> patternMap = new java.util.HashMap<>();
 
-        public CharacterPattern(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
-
-        public String[] getPattern() {
-            return pattern;
-        }
+    static {
+        patternMap.put('O', new String[]{
+            "  OOO  ",
+            " O   O ",
+            " O   O ",
+            " O   O ",
+            "  OOO  "
+        });
+        patternMap.put('P', new String[]{
+            " PPPP  ",
+            " P   P ",
+            " PPPP  ",
+            " P     ",
+            " P     "
+        });
+        patternMap.put('S', new String[]{
+            "  SSSS ",
+            " S     ",
+            "  SSS  ",
+            "     S ",
+            " SSSS  "
+        });
     }
 
-   
-    private static class CharacterPatternMap {
-        public static CharacterPattern getCharacterPattern(char c) {
-            return switch (Character.toUpperCase(c)) {
-                case 'O' -> new CharacterPattern('O', new String[]{
-                    "  OOO  ",
-                    " O   O ",
-                    " O   O ",
-                    " O   O ",
-                    "  OOO  "
-                });
-                case 'P' -> new CharacterPattern('P', new String[]{
-                    " PPPP  ",
-                    " P   P ",
-                    " PPPP  ",
-                    " P     ",
-                    " P     "
-                });
-                case 'S' -> new CharacterPattern('S', new String[]{
-                    "  SSSS ",
-                    " S     ",
-                    "  SSS  ",
-                    "     S ",
-                    " SSSS  "
-                });
-                default -> new CharacterPattern(c, new String[]{" ", " ", " ", " ", " "});
-            };
-        }
-    }
-
-   
     public void generateBanner(String text) {
         int height = 5; 
         StringBuilder bannerBuilder = new StringBuilder();
 
         for (int i = 0; i < height; i++) {
             for (char c : text.toCharArray()) {
-                CharacterPattern cp = CharacterPatternMap.getCharacterPattern(c);
-                bannerBuilder.append(cp.getPattern()[i]).append("  "); 
+                String[] pattern = patternMap.get(Character.toUpperCase(c));
+                if (pattern == null) {
+                    pattern = new String[]{" ", " ", " ", " ", " "};
+                }
+                bannerBuilder.append(pattern[i]).append("  "); 
             }
             bannerBuilder.append("\n");
         }
@@ -61,7 +45,7 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
         OOPSBannerApp app = new OOPSBannerApp();
-        System.out.println("--- UC7: Object-Oriented Banner ---");
+        System.out.println("--- UC8: Map-Based Banner ---");
         app.generateBanner("OOPS");
     }
 }
